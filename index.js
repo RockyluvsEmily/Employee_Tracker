41d99098ddd750403e9e04f09866cb4d1dfb8012
Dewayne Cavendish <dcavendish312@gmail.com>
2023-06-19 08:35:12 -0500

    finished the schema tables

--- index.js
+++ index.js
@@ -50,30 +50,125 @@ function startApp() {
 };
 
 function viewDepartments(){
-
+    connection.query('select * from department', (err,res) => {
+        if(err) throw err
+        console.table(res)
+        startApp()
+    })
 }
 
 function viewRoles() {
-
+    connection.query('select * from role', (err, res) => {
+        if (err) throw err
+        console.table(res)
+        startApp()
+    })
 }
 
 function viewEmployees() {
-
+    connection.query('select * from employee', (err, res) => {
+        if (err) throw err
+        console.table(res)
+        startApp()
+    })
 }
 
 function addDepartment() {
-
+    inquirer.prompt([
+        {
+            name: "addDept",
+            message: "What is the name of the new department?"
+        }
+    ]).then(function (answer) {
+        connection.query(
+            "INSERT INTO department SET ?", {
+            name: answer.addDept
+        },
+            function (err, res) {
+                if (err) throw err;
+                console.log(" Department Added");
+                startApp();
+            }
+        );
+    });
+    
 }
 
 function addRole() {
-
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        //asking for the three properties on the roles table      
+        inquirer.prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the title of the new role?"
+            },
+            {
+                name: "salary",
+                type: "number",
+                message: "What is the salary of this position?",
+            },
+            {
+                name: "deptId",
+                type: "list",
+                message: "Select a department for this role",
+                choices: res.map(item => item.name)
+            }
+        ]).then(function (answers) {
+            const selectedDept = res.find(dept => dept.name === answers.deptId);
+            connection.query("INSERT INTO role SET ?",
+                {
+                    title: answers.title,
+                    salary: answers.salary,
+                    dept_id: selectedDept.id
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("New role added");
+                    startApp();
+                }
+            );
+        });
+    })
 }
 
 function addEmployee() {
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        //asking for the first and last names of the people      
+        inquirer.prompt([
+            {
+                first_name: "name",
+                type: "input",
+                message: "What is your first name?",
+            },
+            {
+                last_name: "name",
+                type: "input",
+                message: "What is your last name?",
+            },
+    
+        ]).then(function (answers) {
+            const selectedDept = res.find(dept => dept.name === answers.deptId);
+            connection.query("INSERT INTO role SET ?",
+                {
+                    title: answers.title,
+                    salary: answers.salary,
+                    dept_id: selectedDept.id
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("New role added");
+                    startApp();
+                }
+            );
+        });
+    })
 
 }
 
 function employeeUpdate() {
-    
+
 }
 startApp()
